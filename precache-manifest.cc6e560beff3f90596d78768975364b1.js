925ddbd231d0bd920c0c41c4759eec5006fc0da6
Heitor Saldanha <heitorsaldanha@gmail.com>
2020-05-06 20:11:02 +0100

    Updates

---
+++ precache-manifest.cc6e560beff3f90596d78768975364b1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dd78ebf9cbfcc9ae5b944c82be4aaf0",
+    "url": "/capeta.io/index.html"
+  },
+  {
+    "revision": "385cf44c6242ccd7df68",
+    "url": "/capeta.io/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "928094e629bafc41298a",
+    "url": "/capeta.io/static/css/main.5374fe50.chunk.css"
+  },
+  {
+    "revision": "385cf44c6242ccd7df68",
+    "url": "/capeta.io/static/js/2.120d83ac.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/capeta.io/static/js/2.120d83ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "928094e629bafc41298a",
+    "url": "/capeta.io/static/js/main.993dd1e7.chunk.js"
+  },
+  {
+    "revision": "6b1c258cb9adafb6b508",
+    "url": "/capeta.io/static/js/runtime-main.23071c99.js"
+  },
+  {
+    "revision": "67d7ad84e66d7c5b901a16c8571df14e",
+    "url": "/capeta.io/static/media/pentagram.67d7ad84.svg"
+  }
+]);
