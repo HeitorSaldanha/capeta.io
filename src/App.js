e7709b6752f64ed0a4eb0af526d04e67ef5eb818
Heitor Saldanha <heitorsaldanha@gmail.com>
2020-05-06 18:12:28 +0100

    Criado JSON de prefixos e melhorias de layout

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import React from 'react';
 import './App.css';
+import React from 'react';
+
 import nomes from './nomes.json';
+import prefixos from './prefixos.json';
+
+import Card from 'react-bootstrap/Card'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <ul>
-      {
-        <li>{nomes[Math.floor(Math.random() * nomes.length)].nome}</li>
-      }
-      </ul>
+        <Card style={{ width: '18rem' }}>
+          <Card.Body>
+            <Card.Title>
+            {
+              prefixos[Math.floor(Math.random() * prefixos.length)].prefix
+            }
+            </Card.Title>
+            <Card.Text>
+            {
+              nomes[Math.floor(Math.random() * nomes.length)].nome
+            }!
+            </Card.Text>
+          </Card.Body>
+        </Card>
     </div>
   );
 }
