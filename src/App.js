45373407deb5d2c3db278695a649d16bcb31ace6
Heitor Saldanha <heitorsaldanha@gmail.com>
2020-05-06 19:22:44 +0100

    Melhorias de estilo e inclusão de vetor de pentagrama

--- src/App.js
+++ src/App.js
@@ -4,26 +4,46 @@ import React from 'react';
 import nomes from './nomes.json';
 import prefixos from './prefixos.json';
 
-import Card from 'react-bootstrap/Card'
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Pentagram from './pentagram.svg';
 
 function App() {
   return (
     <div className="App">
-        <Card style={{ width: '18rem' }}>
-          <Card.Body>
-            <Card.Title>
-            {
-              prefixos[Math.floor(Math.random() * prefixos.length)].prefix
-            }
-            </Card.Title>
-            <Card.Text>
-            {
-              nomes[Math.floor(Math.random() * nomes.length)].nome
-            }!
-            </Card.Text>
-          </Card.Body>
-        </Card>
+      <Container>
+        <img
+          src={Pentagram}
+          alt="Pentagrama"
+          style={{
+            maxWidth: '30%',
+            margin: '40px'
+          }}
+        />
+        <div className="row justify-content-center">
+          <Card
+            style={{
+              background: "#c41b21",
+              color: "black"
+            }}
+          >
+            <Card.Body>
+              <Card.Title>
+              {
+                prefixos[Math.floor(Math.random() * prefixos.length)].prefix
+              }
+              </Card.Title>
+              <Card.Text>
+              {
+                nomes[Math.floor(Math.random() * nomes.length)].nome
+              }!
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
+      </Container>
     </div>
   );
 }
